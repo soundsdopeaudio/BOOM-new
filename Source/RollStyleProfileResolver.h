dc5fef78d13028942654497578fa9f6847a295b7
Sounds Dope Audio <incorporatedRx@gmail.com>
2025-12-21 01:33:18 -0800

    Delete Source directory

--- Source/RollStyleProfileResolver.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "RollStyleRuleSet.h"
-#include <juce_core/juce_core.h>
-
-RollStyleRuleSet resolveRollRules(const juce::String& style);
--- Source/RollStyleProfiles.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "RollStyleRuleSet.h"
-
-// Central resolver for roll style behavior
-RollStyleRuleSet resolveRollRules(const juce::String& style);
